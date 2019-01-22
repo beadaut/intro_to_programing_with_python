8287d596b89ad8a1c3c1091daea78fd528d93010
toluwajosh <tjosh.owoyemi@gmai.com>
2019-01-22 14:43:41 +0900

    finished writing outline

--- intro_to_python_1.md
+++ intro_to_python_1.md
@@ -1,2 +1,36 @@
-# Introduction to Programming wity Python
+# Introduction to Programming using Python
 
+Introduction to Programming using Python
+
+## Outline
+
+- Writing simple programs (introduction to elements of a python program)
+  - Print Statements
+  - Strings
+  - Variables
+  - Comments
+  - Arithmetic Operations
+  - Booleans
+- Iterables
+  - list
+  - tuples
+  - strings as iterables
+- Control Structures
+  - If/else/elif
+  - For Loops
+  - While Loops
+- Data Structures Intoduction (Dictionaries)
+- Using Functions
+
+## Useful resources
+
+https://www.pythonlikeyoumeanit.com/index.html
+
+<!-- page break -->
+<div style="page-break-after: always;"></div>
+
+### Sample code snippet
+
+```python
+print("Hello World!")
+```
--- programs/tkinter_gui.py
+++ programs/tkinter_gui.py
@@ -11,19 +11,22 @@ import tkinter.messagebox as tkMessageBox
 
 top = tkinter.Tk()
 
-# %% buttons
+# %% buttons and message boxes
+# https://www.tutorialspoint.com/python/tk_button.htm
+# https://www.tutorialspoint.com/python/tk_pack.htm
 
 def helloCallBack():
    tkMessageBox.showinfo( "Hello Python", "Hello World")
 
-B = tkinter.Button(top, text ="Hello", command = helloCallBack)
+B = tkinter.Button(top, text ="Hello", command = helloCallBack, fg="blue")
 D = tkinter.Button(top, text ="How are you doing?", command = helloCallBack)
 
-B.pack()
-D.pack()
+B.pack(side=tkinter.LEFT)
+#D.pack()
 #top.mainloop()
 
 # %% canvas
+# https://www.tutorialspoint.com/python/tk_canvas.htm
 
 C = tkinter.Canvas(top, bg="blue", height=250, width=300)
 
@@ -31,4 +34,25 @@ coord = 10, 50, 240, 210
 arc = C.create_arc(coord, start=0, extent=150, fill="red")
 
 #C.pack()
+
+
+# %% message
+# https://www.tutorialspoint.com/python/tk_message.htm
+var = tkinter.StringVar() # you can change the content of the variable with time
+label = tkinter.Message(top, text="Where are you now? \nI know where I am but do you?")
+#label = tkinter.Message(top, textvariable=var)
+#label = tkinter.Message( top, textvariable=var, relief=tkinter.RAISED )
+
+var.set("Hey!?\nHow are you doing?")
+label.pack()
+
+# %% grid: arrange widget in grids
+#for r in range(3):
+#   for c in range(4):
+#      tkinter.Label(top, text='R%s/C%s'%(r,c),
+#         borderwidth=1 ).grid(row=r,column=c)
+
+# %%
+B.place(bordermode=tkinter.OUTSIDE, height=100, width=100)
+
 top.mainloop()
